4f0242236309c975a1a3d3064a41cdfa4a585c4c
Yanfeng Lee <liyafe1997@126.com>
2024-08-02 13:48:38 +0000

    Add buildall script

---
+++ buildall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+bash build.sh alioth
+bash build.sh cmi
+bash build.sh enuma
+bash build.sh munch
+bash build.sh apollo
+bash build.sh dagu
+bash build.sh pipa
+bash build.sh thyme
+bash build.sh cas
+bash build.sh elish
+bash build.sh lmi
+bash build.sh psyche
+bash build.sh umi
+
+bash build.sh alioth ksu
+bash build.sh cmi ksu
+bash build.sh enuma ksu
+bash build.sh munch ksu
+bash build.sh apollo ksu
+bash build.sh dagu ksu
+bash build.sh pipa ksu
+bash build.sh thyme ksu
+bash build.sh cas ksu
+bash build.sh elish ksu
+bash build.sh lmi ksu
+bash build.sh psyche ksu
+bash build.sh umi ksu
