939896e43ad347b85c749c8decb7b9a00e367d9f
Yanfeng Lee <liyafe1997@126.com>
2024-08-25 22:32:20 +0800

    Fix WLAN config problem for LineageOS driver

--- drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_misc.h
+++ drivers/staging/qcacld-3.0/core/hdd/inc/wlan_hdd_misc.h
@@ -42,6 +42,7 @@
 #endif
 
 #ifdef MSM_PLATFORM
+#define WLAN_INI_FILE_DEFAULT      "wlan/qca_cld/qca6390/" PREFIX "WCNSS_qcom_cfg.ini"
 #define WLAN_INI_FILE              "wlan/qca_cld/" PREFIX "WCNSS_qcom_cfg.ini"
 #define WLAN_MAC_FILE              "wlan/qca_cld/" PREFIX "wlan_mac.bin"
 #else
