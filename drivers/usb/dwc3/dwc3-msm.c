755bea013ad0ef7f48689675b506de4048b85286
Yanfeng Lee <liyafe1997@126.com>
2024-09-10 19:46:18 +0000

    Fix build problem This idea is pick from 96ee2b445dc4a8 of UtsavBalar1231's repo.

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -4394,7 +4394,9 @@ static void msm_dwc3_perf_vote_work(struct work_struct *w)
 			msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 }
 
+#if defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_NO_PS_USB3)
 extern bool has_dp_flag;
+#endif
 
 #define VBUS_REG_CHECK_DELAY	(msecs_to_jiffies(1000))
 
@@ -4514,8 +4516,10 @@ static int dwc3_otg_start_host(struct dwc3_msm *mdwc, int on)
 		schedule_delayed_work(&mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 
+#if defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_NO_PS_USB3)
 		if (!has_dp_flag)
 			ps5169_cfg_usb();
+#endif
 
 	} else {
 		dev_dbg(mdwc->dev, "%s: turn off host\n", __func__);
@@ -4637,8 +4641,10 @@ static int dwc3_otg_start_peripheral(struct dwc3_msm *mdwc, int on)
 		schedule_delayed_work(&mdwc->perf_vote_work,
 				msecs_to_jiffies(1000 * PM_QOS_SAMPLE_SEC));
 
+#if defined(CONFIG_BOARD_DAGU) && !defined(CONFIG_NO_PS_USB3)
 		if (!has_dp_flag)
 			ps5169_cfg_usb();
+#endif
 
 	} else {
 		dev_dbg(mdwc->dev, "%s: turn off gadget %s\n",
