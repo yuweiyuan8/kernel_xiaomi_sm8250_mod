7dc8ce08200f3600aaaa241ccdfcb3824dbe3fa7
Yanfeng Lee <liyafe1997@126.com>
2024-09-29 00:36:24 +0000

    Add xiaomi tablets to buildall.sh

--- buildall.sh
+++ buildall.sh
@@ -10,6 +10,10 @@ bash build.sh cmi
 bash build.sh cas
 bash build.sh apollo
 bash build.sh alioth
+bash build.sh elish
+bash build.sh enuma
+bash build.sh dagu
+bash build.sh pipa
 
 bash build.sh psyche ksu
 bash build.sh thyme ksu
@@ -20,3 +24,7 @@ bash build.sh cmi ksu
 bash build.sh cas ksu
 bash build.sh apollo ksu
 bash build.sh alioth ksu
+bash build.sh elish ksu
+bash build.sh enuma ksu
+bash build.sh dagu ksu
+bash build.sh pipa ksu
